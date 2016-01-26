5836f3a752e7b21aa763682228f8e961d6e0002b
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2016-01-25 19:19:42 -0600

    Corregir error al cargar datos desde archivos de excel

--- src/MINSAL/IndicadoresBundle/Excel/Excel.php
+++ src/MINSAL/IndicadoresBundle/Excel/Excel.php
@@ -5,26 +5,5 @@ use PHPExcel_IOFactory;
 use OS\ExcelBundle\Excel\Excel as ExcelBase;
 
 class Excel extends ExcelBase
-{
-    /**
-     * Parse and load and spreadsheet file
-     *
-     * @param string $file
-     * @param array  $options
-     */
-    public function loadFile($file = null, $options = array())
-    {
-        $this->options += $options;
-
-        if (!$file)
-            throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException('Missing arguments!');
-
-        extract($this->options);
-
-        $this->reader = PHPExcel_IOFactory::createReader(PHPExcel_IOFactory::identify($file));
-        //$this->reader->setReadDataOnly($readOnly);
-        $this->phpExcel = $this->reader->load($file);
-
-        $this->setActiveSheet();
-    }
+{    
 }
