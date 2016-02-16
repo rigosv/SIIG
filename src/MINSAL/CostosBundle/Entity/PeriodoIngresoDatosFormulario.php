86ff5c8546ce3e2191c1cc407497d80a0c7478d4
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2016-02-15 21:34:34 -0600

    Cambio en la forma en que se generar el campo autoincremental de periodo_ingreso_datos_formulario

--- src/MINSAL/CostosBundle/Entity/PeriodoIngresoDatosFormulario.php
+++ src/MINSAL/CostosBundle/Entity/PeriodoIngresoDatosFormulario.php
@@ -17,7 +17,7 @@ class PeriodoIngresoDatosFormulario
      *
      * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      * @ORM\SequenceGenerator(sequenceName="costos.ingreso_datos_formulario_seq")
      */
     private $id;
