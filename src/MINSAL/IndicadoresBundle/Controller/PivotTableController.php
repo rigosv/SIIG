954ad8b5cf78d64f59a67ac3659cda3db30a23bb
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2016-07-10 22:17:03 -0600

    Agregada característica para guardar el estado de la tabla pivote

---
+++ src/MINSAL/IndicadoresBundle/Controller/PivotTableController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MINSAL\IndicadoresBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class PivotTableController extends Controller {
+
+    
+    /**
+     * @Route("/pivotable/guardar_estado/", name="pivotable_guardar_estado", options={"expose"=true})
+     */
+    public function guardarEstadoAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $response = new Response();
+        
+        $tipoElemento = $request->get('tipoElemento');
+        $idElemento = $request->get('id');
+        //json_encode sobre json_decode, quita los saltos de línea que trae configuración
+        $configuracion = json_encode(json_decode($request->get('configuracion')));
+        
+        $usuario = $this->get('security.token_storage')->getToken()->getUser();
+        
+        $em->getRepository("IndicadoresBundle:FichaTecnica")->setConfPivoTable($tipoElemento, $idElemento, $usuario->getId(), $configuracion);
+        
+        return $response;
+    }
+
+    /**
+     * @Route("/pivotable/obtener_estado/", name="pivotable_obtener_estado", options={"expose"=true})
+     */
+    public function obtenerEstadoAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $response = new Response();
+        
+        $tipoElemento = $request->get('tipoElemento');
+        $idElemento = $request->get('id');
+        $usuario = $this->get('security.token_storage')->getToken()->getUser();
+        
+        $conf = $em->getRepository("IndicadoresBundle:FichaTecnica")->getConfPivoTable($tipoElemento, $idElemento, $usuario->getId());
+        //$conf = str_replace('\\\\', '', $conf['configuracion']);
+        $response->setContent($conf['configuracion']);
+        return $response;
+    }
+
+}
--- src/MINSAL/IndicadoresBundle/Resources/public/js/FichaTecnicaAdmin/pivotTable.js
+++ src/MINSAL/IndicadoresBundle/Resources/public/js/FichaTecnicaAdmin/pivotTable.js
@@ -2,6 +2,11 @@ var idIndicadorActivo;
 google.load("visualization", "1", {packages: ["corechart", "charteditor"]});
 
 $(document).ready(function() {
+    var datos_ = '';
+    var configuracion = '';
+    var configuracion_guardada = '';
+    var tipoElemento = '';
+    var identificadorElemento = '';
     $('#myTab a').click(function(e) {
         e.preventDefault();
         $(this).tab('show');
@@ -24,6 +29,41 @@ $(document).ready(function() {
         $('#myModal2').modal('show');
     });
     
+    $('#guardarConf').click(function (){
+        configuracion_guardada = configuracion;
+        var conf = JSON.stringify(configuracion_guardada, undefined, 2);
+        $.post(Routing.generate('pivotable_guardar_estado', 
+                {tipoElemento: tipoElemento, id:identificadorElemento, configuracion: conf}), 
+            function(datos) {
+            
+        }, 'json');
+    });
+    
+    $('#cargarConf').click(function (){
+        
+        var renderers = $.extend($.pivotUtilities.renderers,
+            $.pivotUtilities.gchart_renderers);
+        if (configuracion_guardada == ''){
+           $.post(Routing.generate('pivotable_obtener_estado', 
+                {tipoElemento: tipoElemento, id:identificadorElemento}), 
+            function(conf) {
+                if (conf === ''){
+                    alert('No existe una configuración guardada');
+                }else {
+                    configuracion_guardada = conf;
+                    configuracion_guardada["renderers"] = renderers;
+                    configuracion_guardada["onRefresh"] = onChangeTable;
+                    $("#output").pivotUI(datos_, configuracion_guardada , true, 'es');
+                }
+        }, 'json');
+        } else{
+            configuracion_guardada["renderers"] = renderers;
+            configuracion_guardada["onRefresh"] = onChangeTable;
+            $("#output").pivotUI(datos_, configuracion_guardada , true, 'es');
+        }
+        
+    });
+    
     $('#ver_ficha').click(function() {
         if (idIndicadorActivo != null){ 
             $.get(Routing.generate('get_indicador_ficha',
@@ -42,18 +82,12 @@ $(document).ready(function() {
     $('A.indicador').click(function() {
         var id_indicador = $(this).attr('data-id');
         var nombre_indicador = $(this).html();
-        //var derivers = $.pivotUtilities.derivers;
-
-        var renderers = $.extend($.pivotUtilities.renderers,
-                $.pivotUtilities.gchart_renderers);
-                
         
         $.getJSON(Routing.generate('get_datos_indicador', {id: id_indicador}), function(mps) {
-            $("#output").pivotUI(mps, {
-                renderers: renderers,
-                menuLimit: 500,
-                unusedAttrsVertical: false
-            }, false, 'es');
+            datos_ = mps;
+            tipoElemento = 'indicador';
+            identificadorElemento = id_indicador;
+            cargarTablaDinamica(mps);
             $('#marco-sala').attr('data-content', nombre_indicador);
             $('#myTab a:first').tab('show');
             idIndicadorActivo = id_indicador;
@@ -63,18 +97,12 @@ $(document).ready(function() {
     $('A.elemento_costeo').click(function() {
         var codigo = $(this).attr('data-id');
         var nombre_elemento = $(this).html();
-        //var derivers = $.pivotUtilities.derivers;
-
-        var renderers = $.extend($.pivotUtilities.renderers,
-                $.pivotUtilities.gchart_renderers);
-                
         
         $.getJSON(Routing.generate('get_datos_costeo', {codigo: codigo}), function(mps) {
-            $("#output").pivotUI(mps, {
-                renderers: renderers,
-                menuLimit: 500,
-                unusedAttrsVertical: false
-            }, false, 'es');
+            datos_ = mps;
+            tipoElemento = 'costeo';
+            identificadorElemento = codigo;
+            cargarTablaDinamica(mps);
             $('#marco-sala').attr('data-content', nombre_elemento);
             $('#myTab a:first').tab('show');
         });
@@ -83,27 +111,43 @@ $(document).ready(function() {
     $('A.formulario_captura_datos').click(function() {
         var codigo = $(this).attr('data-id');
         var nombre_elemento = $(this).html();
-        //var derivers = $.pivotUtilities.derivers;
 
-        var renderers = $.extend($.pivotUtilities.renderers,
-                $.pivotUtilities.gchart_renderers);
-                
-        
         $.getJSON(Routing.generate('get_datos_formulario_captura', {codigo: codigo}), function(mps) {
             if (mps.estado == 'error'){
                 $('#output').html('<div class="alert alert-warning" role="alert">'+mps.msj+'</div>');
             }
             else {
-                $("#output").pivotUI(mps, {
-                    renderers: renderers,
-                    menuLimit: 500,
-                    unusedAttrsVertical: false
-                }, false, 'es');
+                datos_ = mps;
+                tipoElemento = 'formulario';
+                identificadorElemento = codigo;
+                cargarTablaDinamica(mps);
             }
             $('#marco-sala').attr('data-content', nombre_elemento);   
             $('#myTab a:first').tab('show');
         });
     });
+    function cargarTablaDinamica(datos){
+        var renderers = $.extend($.pivotUtilities.renderers,
+            $.pivotUtilities.gchart_renderers);
+                
+        $("#output").pivotUI(datos, {
+            renderers: renderers,
+            menuLimit: 500,
+            unusedAttrsVertical: false,
+            onRefresh: onChangeTable
+        }, false, 'es');
+    }
+    
+    var onChangeTable = (function(config) {
+                var config_copy = JSON.parse(JSON.stringify(config));
+                //delete some values which are functions
+                delete config_copy["aggregators"];
+                delete config_copy["renderers"];
+                //delete some bulky default values
+                delete config_copy["rendererOptions"];
+                delete config_copy["localeStrings"];
+                configuracion = config_copy;
+            });   
 });
 
 var tableToExcel = (function() {
