5a65f10aceaf5bcff98ef71424d49a90d2764268
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2016-08-25 23:18:14 -0600

    Primer bosquejo de matriz de seguimiento

--- src/MINSAL/IndicadoresBundle/Controller/ReportesController.php
+++ src/MINSAL/IndicadoresBundle/Controller/ReportesController.php
@@ -16,10 +16,46 @@ class ReportesController extends Controller {
      */
     public function matrizSeguimientoAccion() {
         $admin_pool = $this->get('sonata.admin.pool');
+        
+        $em = $this->getDoctrine()->getManager();
+        
+        $Frm = $em->find('GridFormBundle:Formulario', 12);
+        $datosFrm = $em->getRepository('GridFormBundle:Formulario')->getDatosRAW($Frm);
+
+        $params = array('2015'=>array('01', '02', '03'), '2016'=>array('01','02','03','04','05'));
+        
+        $datos_ = array();
+        foreach ($datosFrm as $f){
+            if (array_key_exists($f['anio'], $params)){
+                
+                foreach ($f as $k => $sf){
+                    $mesVarR = str_replace('cant_mensual_calidad_', '', $k);
+                    if (in_array($mesVarR, $params[$f['anio']])){
+                        $datos_['real'][$mesVarR.'/'.$f['anio']] = $sf;
+                    }
+                    $mesVarP = str_replace('_p', '', str_replace('cant_mensual_calidad_', '', $k));
+                    if (in_array($mesVarP, $params[$f['anio']])){
+                        $datos_['planificado'][$mesVarP.'/'.$f['anio']] = $sf;
+                    }
+                }
+                if (count($datos_['real']) > 0){
+                    foreach ($datos_['planificado'] as $k=>$v){
+                        $datos_['estatus'][$k] = ($v > 0) ? number_format(($datos_['real'][$k] / $v) * 100,0) : null; 
+                    }
+                    $datosFrmFormat[$f['codigo_variable']]['datos']= $datos_;
+                    $datosFrmFormat[$f['codigo_variable']]['descripcion'] = $f['descripcion_variable'];
+                    $datosFrmFormat[$f['codigo_variable']]['categoria'] = $f['descripcion_categoria_variable'];
+                }
+            }
+        }
+        
+        var_dump($datosFrmFormat['90_expe_revisados']['datos']);
 
         return $this->render('IndicadoresBundle:Reportes:matrizSeguimiento.html.twig', 
                                 array(
-                                    'admin_pool' => $admin_pool
+                                    'admin_pool' => $admin_pool,
+                                    'datosFrm' => $datosFrmFormat,
+                                    'parms' => $params
                                 ));
     }
 }
