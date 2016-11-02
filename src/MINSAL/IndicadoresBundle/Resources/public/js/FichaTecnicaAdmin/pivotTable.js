df13d19442c22a6c290b29da39211789387d855a
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2016-11-01 20:45:50 -0600

    Arreglo de caracteres especiales al exportar a excel

--- src/MINSAL/IndicadoresBundle/Resources/public/js/FichaTecnicaAdmin/pivotTable.js
+++ src/MINSAL/IndicadoresBundle/Resources/public/js/FichaTecnicaAdmin/pivotTable.js
@@ -20,8 +20,11 @@ $(document).ready(function() {
     }
     ajax_states();
     $('#export').click(function(){
-       var t = $('.pvtTable');
-        tableToExcel(t[0],'indicador', $('#marco-sala').attr('data-content').trim()+'.xls');
+        $('.pvtTable').table2excel({
+                exclude: ".excludeThisClass",
+                name: $('#marco-sala').attr('data-content'),
+                filename: $('#marco-sala').attr('data-content').trim() //do not include extension
+            });
     });
     $('#export_grp').click(function() {        
         var html = $('div.c3').html();
@@ -182,19 +185,3 @@ $(document).ready(function() {
                 configuracion = config_copy;
             });   
 });
-
-var tableToExcel = (function() {
-  var uri = 'data:application/vnd.ms-excel;base64,'
-    , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
-    , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
-    , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
-  return function(table, name, filename) {
-    if (table !== undefined){        
-        if (!table.nodeType) table = document.getElementById(table);
-        var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML};
-        document.getElementById("dlink").href = uri + base64(format(template, ctx));
-            document.getElementById("dlink").download = filename;
-            document.getElementById("dlink").click();
-    }
-}
-})();
