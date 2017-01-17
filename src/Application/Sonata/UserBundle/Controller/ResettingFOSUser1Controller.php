9f63dc47653e5a94ea9a0fdaaade7d91ff8d2833
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2017-01-17 16:55:12 -0600

    Configuración envio correo para restablecer contraseña

--- src/Application/Sonata/UserBundle/Controller/ResettingFOSUser1Controller.php
+++ src/Application/Sonata/UserBundle/Controller/ResettingFOSUser1Controller.php
@@ -11,6 +11,7 @@
 
 namespace Application\Sonata\UserBundle\Controller;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use FOS\UserBundle\Controller\ResettingController;
 
 /**
@@ -27,5 +28,37 @@ class ResettingFOSUser1Controller extends ResettingController
     public function requestAction()
     {
         return $this->container->get('templating')->renderResponse('SonataUserBundle:Resetting:request.html.'.$this->getEngine());
+    }
+    
+    /**
+     * Request reset user password: submit form and send email
+     */
+    public function sendEmailAction()
+    {
+        $username = $this->container->get('request')->request->get('username');
+
+        /** @var $user UserInterface */
+        $user = $this->container->get('fos_user.user_manager')->findUserByUsernameOrEmail($username);
+
+        if (null === $user) {
+            return $this->container->get('templating')->renderResponse('FOSUserBundle:Resetting:request.html.'.$this->getEngine(), array('invalid_username' => $username));
+        }
+
+        if ($user->isPasswordRequestNonExpired($this->container->getParameter('fos_user.resetting.token_ttl'))) {
+            return $this->container->get('templating')->renderResponse('FOSUserBundle:Resetting:passwordAlreadyRequested.html.'.$this->getEngine());
+        }
+
+        if (null === $user->getConfirmationToken() or $user->getConfirmationToken() == '') {
+            /** @var $tokenGenerator \FOS\UserBundle\Util\TokenGeneratorInterface */
+            $tokenGenerator = $this->container->get('fos_user.util.token_generator');
+            $user->setConfirmationToken($tokenGenerator->generateToken());
+        }
+        
+        $this->container->get('session')->set(static::SESSION_EMAIL, $this->getObfuscatedEmail($user));
+        $this->container->get('fos_user.mailer')->sendResettingEmailMessage($user);
+        $user->setPasswordRequestedAt(new \DateTime());
+        $this->container->get('fos_user.user_manager')->updateUser($user);
+
+        return new RedirectResponse($this->container->get('router')->generate('fos_user_resetting_check_email'));
     }
 }
