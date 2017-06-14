db54e7e2eec6e39c6262065eeb58208787cea192
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2017-06-14 00:57:09 -0600

    Corrección de comportamiento que no dejaba ingresar datos por primera vez a un formulario

--- src/MINSAL/GridFormBundle/Controller/GridController.php
+++ src/MINSAL/GridFormBundle/Controller/GridController.php
@@ -94,10 +94,11 @@ class GridController extends Controller
             }
             $data_ = json_encode($this->setPorcentajeCompletado($datos), JSON_UNESCAPED_UNICODE);
             $user = $this->getUser();
+            $datosColumna = array();
             if ($Frm->getFormaEvaluacion() == 'lista_chequeo'){
                 //Guardar solo el valor de la columna, no se hace con todos
                 // los formularios porque los otros, pueden traer columnas calculadas
-                $datosColumna[$request->get('columna')] =  $request->get('valor');                
+                $datosColumna[$request->get('columna')] =  $datos[$request->get('columna')];
                 $datosColumna[$request->get('pk')] =  $datos[$request->get('pk')];
                 
                 $data_ = json_encode( $datosColumna);
